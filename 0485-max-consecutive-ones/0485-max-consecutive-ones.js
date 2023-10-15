3718962abe39e2c3727e291121e2af637751d04c
Prashant Kumar <106888124+psant-kr@users.noreply.github.com>
2023-10-15 16:25:00 +0530

    Time: 63 ms (37.56%), Space: 44.3 MB (76.04%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let count = 0;
+    let max = 0;
+
+        for(let i = 0; i<nums.length; i++) {
+            if(nums[i] == 1){
+                // count++;
+                count+=1;
+                max = Math.max(count, max);
+            }else{
+                count = 0;
+            }
+        }
+        return max;
+};
