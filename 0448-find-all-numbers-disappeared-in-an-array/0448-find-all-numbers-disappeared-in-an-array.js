22bd324c838fa8efc68a97c7709cb5a9ead35e60
Prashant Kumar <106888124+psant-kr@users.noreply.github.com>
2023-10-16 01:15:09 +0530

    Time: 82 ms (81.42%), Space: 49.7 MB (87.15%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -4,7 +4,7 @@
  */
 var findDisappearedNumbers = function(nums) {
   // do not use extra array or object or any inbuilt method.
-    for(let i = 0; i <= nums.length; i++){
+    for(let i = 0; i < nums.length; i++){
         let element = Math.abs(nums[i]);
         let idelement = element - 1;
         if(nums[idelement] > 0){
@@ -12,7 +12,7 @@ var findDisappearedNumbers = function(nums) {
         }      
     }
     let array = [];
-        for(let j=0; j <= nums.length; j++ ){
+        for(let j=0; j < nums.length; j++ ){
             if(nums[j] > 0){
                 array.push(j+1);
             }
