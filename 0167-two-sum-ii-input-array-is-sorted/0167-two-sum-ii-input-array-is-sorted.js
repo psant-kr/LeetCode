3bfdc07aa5af9262850f79f78be01e09964570a1
Prashant Kumar <106888124+psant-kr@users.noreply.github.com>
2023-10-17 00:58:35 +0530

    Time: 66 ms (17.46%), Space: 43.3 MB (7.74%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,12 +4,25 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    for(var i=0; i<=numbers.length-2; i++){
-        for(var j = i+1; j<numbers.length;j++){
-            if(numbers[i]+numbers[j] == target){
-                return [i+1,j+1]           
-            }
+
+// worst answer
+    // for(var i=0; i<=numbers.length-2; i++){
+    //     for(var j = i+1; j<numbers.length;j++){
+    //         if(numbers[i]+numbers[j] == target){
+    //             return [i+1,j+1]           
+    //         }
+    //     }
+    // }
+    // return array
+// Good answer
+    let start = 0, end = numbers.length - 1;
+    while(start < end){
+        if(numbers[start] + numbers[end] == target){
+            return [start+1, end+1];
+        }else if(numbers[start] + numbers[end] > target){
+            end--;
+        }else{
+            start++;
         }
     }
-    return array
 };
