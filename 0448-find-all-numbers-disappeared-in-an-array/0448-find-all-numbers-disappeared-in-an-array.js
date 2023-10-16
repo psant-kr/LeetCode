d0a9cd36fa652dc3a3a97a805f77d2e32fabacd5
Prashant Kumar <106888124+psant-kr@users.noreply.github.com>
2023-10-16 13:04:25 +0530

    Time: 108 ms (35.55%), Space: 52.4 MB (48.61%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -4,18 +4,31 @@
  */
 var findDisappearedNumbers = function(nums) {
   // do not use extra array or object or any inbuilt method.
+    // for(let i = 0; i < nums.length; i++){
+    //     let element = Math.abs(nums[i]);
+    //     let idelement = element - 1;
+    //     if(nums[idelement] > 0){
+    //         nums[idelement] *= -1;
+    //     }      
+    // }
+    // let array = [];
+    //     for(let j=0; j < nums.length; j++ ){
+    //         if(nums[j] > 0){
+    //             array.push(j+1);
+    //         }
+    //     }
+    // return array;
+
+// using extra array and object.
+    let obj = {};
     for(let i = 0; i < nums.length; i++){
-        let element = Math.abs(nums[i]);
-        let idelement = element - 1;
-        if(nums[idelement] > 0){
-            nums[idelement] *= -1;
-        }      
-    }
+        obj[nums[i]] = nums[i];
+    };
     let array = [];
-        for(let j=0; j < nums.length; j++ ){
-            if(nums[j] > 0){
-                array.push(j+1);
-            }
+    for(let i = 1; i <= nums.length; i++){
+        if(i != obj[i]){
+            array.push(i)
         }
+    }
     return array;
 };
