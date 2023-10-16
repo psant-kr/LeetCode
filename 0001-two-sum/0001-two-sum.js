de012850338e41ebef43485396010e5b80159441
Prashant Kumar <106888124+psant-kr@users.noreply.github.com>
2023-10-16 17:23:27 +0530

    Time: 91 ms (30.35%), Space: 42.2 MB (76.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,13 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let array = [];
      for(var i=0;i<nums.length;i++){
         for(var j = i+1;j<nums.length;j++){
             if(nums[i]+nums[j] == target){
-                return [i,j]
+                // return [i,j]
+                array.push(i,j)                
             }
         }
     }
+    return array
 };
 
 
