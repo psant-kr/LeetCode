5709ee16fb2325b08edf9a82fd5eac0cb0caaf3b
Prashant Kumar <106888124+psant-kr@users.noreply.github.com>
2023-10-17 01:34:26 +0530

    Time: 68 ms (54.17%), Space: 44.8 MB (8.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,6 +3,7 @@
  * @param {number} target
  * @return {number[]}
  */
+
 var twoSum = function(nums, target) {
 // my bruteforce solution
 
@@ -19,14 +20,33 @@ var twoSum = function(nums, target) {
 
 // bruteforce exact solution 
      
-     for(var i=0; i<=nums.length-2; i++){
-        for(var j = i+1; j<nums.length;j++){
-            if(nums[i]+nums[j] == target){
-                return [i,j]           
-            }
+    //  for(var i=0; i<=nums.length-2; i++){
+    //     for(var j = i+1; j<nums.length;j++){
+    //         if(nums[i]+nums[j] == target){
+    //             return [i,j]           
+    //         }
+    //     }
+    // }
+    // return array
+
+// best solution
+    for(let i = 0; i < nums.length; i++){
+        nums[i] = [nums[i], i];
+    };
+    function fn(x,y){
+        return x[0] - y[0]; 
+    };
+    nums.sort(fn);
+    let start = 0, end = nums.length - 1;
+    while(start < end){
+        if(nums[start][0] + nums[end][0] == target){
+            return [nums[start][1], nums[end][1]];
+        }else if(nums[start][0] + nums[end][0] > target){
+            end--;
+        }else{
+            start++;
         }
     }
-    return array
 };
 
 
