db0080404933d153e0e26b670393a697a7255830
Prashant Kumar <106888124+psant-kr@users.noreply.github.com>
2023-10-20 23:06:49 +0530

    Time: 69 ms (53.37%), Space: 45.4 MB (46.15%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.js
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.js
@@ -5,6 +5,7 @@
  * @return {number[][]}
  */
 var matrixReshape = function(mat, r, c) {
+/** 
     let result ;
     if((mat.length*mat[0].length) == r*c){
         let Matrix = Array(r);
@@ -29,5 +30,32 @@ var matrixReshape = function(mat, r, c) {
         result = mat;
     }
 
+    return result;
+*/
+
+    let m = mat.length;
+    let n = mat[0].length;
+
+    // cannot do reshape
+    if(m*n != r*c) return mat;
+
+    let row = 0;
+    let col = 0;
+    let result = Array(r);
+    for(let i = 0; i < r; i++){
+        result[i] = Array(c).fill(0);
+    }
+
+    for(let i = 0; i < m; i++){
+        for(let j = 0; j < n; j++){
+            let element = mat[i][j];
+            result[row][col] = element;
+            col++;
+            if(col == c){
+                row++;
+                col = 0;
+            }
+        }
+    }
     return result;
 };
