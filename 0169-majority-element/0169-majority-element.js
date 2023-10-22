bef4e35f6ab1a711263b7f7920d6a1dc999026c2
Prashant Kumar <106888124+psant-kr@users.noreply.github.com>
2023-10-22 16:40:43 +0530

    Time: 65 ms (27.4%), Space: 43.5 MB (76.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,17 +3,33 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let obj = {};
-    for(let i = 0;i < nums.length;i++){
-        if(obj[nums[i]]){
-            obj[nums[i]] += 1;
-        }else{
-            obj[nums[i]] = 1;
+    // here we are using extra space that is not good
+    // let obj = {};
+    // for(let i = 0;i < nums.length;i++){
+    //     if(obj[nums[i]]){
+    //         obj[nums[i]] += 1;
+    //     }else{
+    //         obj[nums[i]] = 1;
+    //     }
+    // }
+    // for (let key in obj) {
+    //     if (obj[key] > nums.length / 2) {
+    //        return key
+    //     }
+    // }
+
+    // here we are using no extra space method is cancelling the other element rhan majority
+    let maj;
+    let count = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(count == 0){
+            maj = nums[i];
+            count++;
+        }else if(count != 0 && maj == nums[i]){
+            count++;
+        }else if(count != 0 && maj != nums[i]){
+            count--;
         }
     }
-    for (let key in obj) {
-        if (obj[key] > nums.length / 2) {
-           return key
-        }
-    }
+    return maj;
 };
