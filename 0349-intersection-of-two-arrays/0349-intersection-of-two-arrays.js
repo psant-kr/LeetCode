264311cfae0ec70fbbfc306af9a156356045638f
Prashant Kumar <106888124+psant-kr@users.noreply.github.com>
2023-10-23 17:10:18 +0530

    Time: 66 ms (7.94%), Space: 45.8 MB (5.59%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    /**
+/**
 
     let obj = {};
     let arr = [];
@@ -20,9 +20,10 @@ var intersection = function(nums1, nums2) {
     }
     return arr;
 
-    */
+ */
 
-    let obj = {};
+/**
+ let obj = {};
     for(let i = 0; i < nums1.length; i++){
         for(let j = 0; j < nums2.length; j++){
             if(nums1[i] == nums2[j]){
@@ -30,6 +31,26 @@ var intersection = function(nums1, nums2) {
             }
         }
     }
+    // Object has O as capital as a keyword.
     return  Object.keys(obj);
-    
+ */
+   
+   let obj1 = {};
+   let obj2 = {};
+   for(let i = 0; i < nums1.length; i++){
+       if(obj1[nums1[i]]){
+           obj1[nums1[i]]++;
+       }else{
+           obj1[nums1[i]] = 1 ;
+       }
+   }
+   console.log(obj1);
+   for(let j = 0; j < nums2.length; j++){
+       if(obj1[nums2[j]]){
+        obj2[nums2[j]] = 1;
+       }
+   }
+   console.log(obj2);
+   console.log(Object.keys(obj2));
+    return Object.keys(obj2)
 };
