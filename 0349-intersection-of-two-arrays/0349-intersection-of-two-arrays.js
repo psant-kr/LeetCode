3466ffe95a06f5aa6199810f448d22218ec36e7d
Prashant Kumar <106888124+psant-kr@users.noreply.github.com>
2023-10-23 16:17:22 +0530

    Time: 55 ms (53.7%), Space: 43.9 MB (29.35%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let obj = {};
+    let arr = [];
+    for(let i = 0; i < nums1.length; i++){
+        for(let j = 0; j < nums2.length; j++){
+            if(nums1[i] == nums2[j]){
+                obj[nums1[i]] = nums1[i];
+            }
+        }
+    }
+    for(let key in obj){
+        arr.push(key);
+    }
+    return arr;
+};
